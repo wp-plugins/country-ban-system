7d7e9e26aeb01ed0ae20b27e44cd980342fada7c
djjmz <djjmz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 22:58:54 +0000

    Small update
    git-svn-id: https://plugins.svn.wordpress.org/country-ban-system/trunk@1109899 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-/*
-Plugin Name: Country Ban System
-Plugin URI: 
-Description: Hide text/link/image in specific country.
-Version: 1.0.1
-Author: Justinas Žvikas
-Author URI: 
-*/
-function cbs_block_country($attr,$content) {
-extract(shortcode_atts(array('ct'=>'ct','message'=>'message'),$attr));
-require_once('geoip.inc');
-$geoip = geoip_open(plugin_dir_path(__FILE__) . 'GeoIP.dat',GEOIP_STANDARD);
-$country = geoip_country_code_by_addr($geoip, $_SERVER['REMOTE_ADDR']);
-$ct = strtoupper($ct);
-preg_match_all("/([^,]+)/", $ct, $pairs);
-$i = 0;
-foreach($pairs as $pair){
-if($pair[$i++] == $country){
-$text = '<br><p align="center"><b>'.$message.'</b></p><br>';
-}
-}
-return isset($text)?$text:$content;
-}
-add_shortcode('cbs_block_country', 'cbs_block_country');
+	/*
+		Plugin Name: Country Ban System
+		Plugin URI: 
+		Description: Hide text/link/image in specific country.
+		Version: 1.0.1
+		Author: Justinas Žvikas
+		Author URI: 
+	*/
+	function cbs_block_country($attr,$content) {
+		extract(shortcode_atts(array('ct'=>'ct','message'=>'message'),$attr));
+		require_once('geoip.inc');
+		$geoip = geoip_open(plugin_dir_path(__FILE__) . 'GeoIP.dat',GEOIP_STANDARD);
+		$country = geoip_country_code_by_addr($geoip, $_SERVER['REMOTE_ADDR']);
+		$ct = strtoupper($ct);
+		preg_match_all("/([^,]+)/", $ct, $pairs);
+		$i = 0;
+		foreach($pairs as $pair){
+			if($pair[$i++] == $country){
+				$text = '<br><p align="center"><b>'.$message.'</b></p><br>';
+			}
+		}
+		return isset($text)?$text:$content;
+	}
+	add_shortcode('cbs_block_country', 'cbs_block_country');
 ?>
